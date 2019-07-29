09f9877d79b523a6559828e825572a515367df01
M.Z. Makos <M.Z.Makos@Pro-2.local>
2019-07-29 16:21:06 -0500

    Revert "merge master branch"
    This reverts commit bcddb66b1d62b860b6f36e31b8dd77cacadeaa29, reversing
    changes made to 0422cda290325d010fdd2235d1e199348e0c1cf8.

--- scf.py
+++ scf.py
@@ -1,17 +1,12 @@
-import numpy as np
-
 class scf():
-    def __init__(self,hamiltonian_matrix,interaction_matrix,density_matrix,chi_tensor,energy_ion,ionic_charge, orbitals_per_atom):
+    def __init__(self,hamiltonian_matrix,interaction_matrix,density_matrix,chi_tensor, energy_ion, converged=False):
         self.hamiltonian_matrix = hamiltonian_matrix
         self.interaction_matrix = interaction_matrix
         self.density_matrix = density_matrix
         self.chi_tensor = chi_tensor
         self.energy_ion = energy_ion
-        self.ionic_charge= ionic_charge
-        self.converged = False
-        self.orbitals_per_atom = orbitals_per_atom
+        self.converged = converged
 
-    '''
     @property
     def fock_matrix(self):
         return self.fock_matrix
@@ -27,7 +22,6 @@ class scf():
     @_density_matrix.setter
     def _density_matrix(self, new_density_matrix):
         self.density_matrix = new_density_matrix
-    '''
 
     def scf_cycle(self, max_scf_iterations = 100,
                 mixing_fraction = 0.25, convergence_tolerance = 1e-4):
@@ -48,19 +42,19 @@ class scf():
     """
         old_density_matrix = self.density_matrix.copy()
         for iteration in range(max_scf_iterations):
-            fock_matrix = self.calculate_fock_matrix(self.hamiltonian_matrix, self.interaction_matrix, old_density_matrix, self.chi_tensor)
-            density_matrix = self.calculate_density_matrix(fock_matrix)
-            error_norm = np.linalg.norm( old_density_matrix - density_matrix)
-            if error_norm < convergence_tolerance:
-                self.converged=True
-                return density_matrix, fock_matrix
+            self.fock_matrix = calculate_fock_matrix(self.hamiltonian_matrix, self.interaction_matrix, old_density_matrix, self.chi_tensor)
+            self.density_matrix = calculate_density_matrix(self.fock_matrix)
 
-            old_density_matrix = (mixing_fraction * density_matrix
+            error_norm = np.linalg.norm( old_density_matrix - self.density_matrix)
+            if error_norm < convergence_tolerance:
+                return self.density_matrix, self.fock_matrix
+
+            old_density_matrix = (mixing_fraction * self.density_matrix
                                 + (1.0 - mixing_fraction) * old_density_matrix)
         print("WARNING: SCF cycle didn't converge")
-        return density_matrix, fock_matrix
+        return self.density_matrix, self.fock_matrix
 
-    def calculate_energy_scf(self,hamiltonian_matrix,fock_matrix,density_matrix):
+    def calculate_energy_scf(self):
         '''Returns the Hartree-Fock total energy defined by the input Hamiltonian, Fock, & density matrices.
 
         Inputs
@@ -78,11 +72,11 @@ class scf():
             Hartree-Fock total energy
 
         '''
-        energy_scf = np.einsum('pq,pq', hamiltonian_matrix + fock_matrix,
-                            density_matrix)
-        return energy_scf
+        self.energy_scf = np.einsum('pq,pq', self.hamiltonian_matrix + self.fock_matrix,
+                            self.density_matrix)
+        return self.energy_scf
 
-    def calculate_density_matrix(self,fock_matrix):
+    def calculate_density_matrix(self):
         '''Returns the 1-electron density matrix defined by the input Fock matrix.
 
             Parameters
@@ -95,14 +89,14 @@ class scf():
 
 
         '''
-        num_occ = (self.ionic_charge // 2) * np.size(fock_matrix,
-                                                0) // self.orbitals_per_atom
-        orbital_energy, orbital_matrix = np.linalg.eigh(fock_matrix)
+        num_occ = (ionic_charge // 2) * np.size(self.fock_matrix,
+                                                0) // orbitals_per_atom
+        orbital_energy, orbital_matrix = np.linalg.eigh(self.fock_matrix)
         occupied_matrix = orbital_matrix[:, :num_occ]
-        density_matrix = occupied_matrix @ occupied_matrix.T
-        return density_matrix
+        self.density_matrix = occupied_matrix @ occupied_matrix.T
+        return self.density_matrix
 
-    def calculate_fock_matrix(self,hamiltonian_matrix,interaction_matrix,density_matrix, chi_tensor):
+    def calculate_fock_matrix(self):
         '''Returns the Fock matrix defined by the input Hamiltonian, interaction, & density matrices.
 
         Parameters
@@ -117,20 +111,20 @@ class scf():
         fock_matrix : np.array
 
         '''
-        fock_matrix = self.hamiltonian_matrix.copy()
-        fock_matrix += 2.0 * np.einsum('pqt,rsu,tu,rs',
+        self.fock_matrix = self.hamiltonian_matrix.copy()
+        sefl.fock_matrix += 2.0 * np.einsum('pqt,rsu,tu,rs',
                                     self.chi_tensor,
                                     self.chi_tensor,
                                     self.interaction_matrix,
                                     self.density_matrix,
                                     optimize=True)
-        fock_matrix -= np.einsum('rqt,psu,tu,rs',
+        self.fock_matrix -= np.einsum('rqt,psu,tu,rs',
                                 self.chi_tensor,
                                 self.chi_tensor,
                                 self.interaction_matrix,
                                 self.density_matrix,
                                 optimize=True)
-        return fock_matrix
+        return self.fock_matrix
 
     def initialize(self):
         self.fock_matrix = self.calculate_fock_matrix(self.hamiltonian_matrix, self.interaction_matrix,
@@ -140,6 +134,6 @@ class scf():
     def kernel(self):
         self.initialize()
         self.density_matrix, self.fock_matrix = self.scf_cycle()
-        self.energy_scf = self.calculate_energy_scf(self.hamiltonian_matrix,self.fock_matrix,self.density_matrix)
-        self.total_energy = self.energy_ion + self.energy_scf
+        self.energy_ion = self.calculate_energy_ion(self.atomic_coordinates)
+        self.total_energy = energy_ion + energy_scf
         return self.total_energy
